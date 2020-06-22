75cc7f721195f7fbcd5b905cae40be5d6fcaaf89
Jeramie Fletcher <jeramie.fletcher@hpe.com>
2020-06-22 17:07:00 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 from distutils.command.install import INSTALL_SCHEMES
-
+from os import path
 
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
@@ -10,11 +10,17 @@ try:
 except ImportError:
     __version__ = 'unknown'
 
+# read the contents of your README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setuptools.setup(
     name='workingdays',
     version=__version__,
     description='A collection of Workingdays date utilities/helper functions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author="Jeramie Fletcher",
     author_email="jeramie.fletcher@gmail.com",
     url="https://github.com/jeramiefletcher/workingdays",
